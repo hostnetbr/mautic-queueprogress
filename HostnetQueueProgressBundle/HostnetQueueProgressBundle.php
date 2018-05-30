8eade368eb0fa17785e8ae325260edd063b81e9a
Henrique Rodrigues <henrique@hostnet.com.br>
2018-05-30 15:01:57 -0300

    removing garbage

--- HostnetQueueProgressBundle/HostnetQueueProgressBundle.php
+++ HostnetQueueProgressBundle/HostnetQueueProgressBundle.php
@@ -15,55 +15,5 @@ class HostnetQueueProgressBundle extends PluginBundleBase
         $metadata = null,
         $installedSchema = null
     ) {
-
-        $db             = $factory->getDatabase();
-        $platform       = $db->getDatabasePlatform()->getName();
-        $queries        = [];
-
-        $queries[] = 'CREATE TABLE IF NOT EXISTS ' . MAUTIC_TABLE_PREFIX . 'plugin_queueprogress_browsers ( id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT , user_id INT(11) NOT NULL , hash VARCHAR(255) NOT NULL , date_added DATETIME NOT NULL , PRIMARY KEY (id))';
-
-        if (!empty($queries)) {
-            $db->beginTransaction();
-            try {
-                foreach ($queries as $q) {
-                    $db->query($q);
-                }
-
-                $db->commit();
-            } catch (\Exception $e) {
-                $db->rollback();
-
-                throw $e;
-            }
-        }
-    }
-
-    public static function onPluginUpdate(
-        Plugin $plugin,
-        MauticFactory $factory,
-        $metadata = null,
-        Schema $installedSchema = null
-    ) {
-        $db             = $factory->getDatabase();
-        $platform       = $db->getDatabasePlatform()->getName();
-        $queries        = array();
-        $fromVersion    = $plugin->getVersion();
-
-        $queries[] = 'CREATE TABLE IF NOT EXISTS ' . MAUTIC_TABLE_PREFIX . 'plugin_queueprogress_browsers ( id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT , user_id INT(11) NOT NULL , hash VARCHAR(255) NOT NULL , date_added DATETIME NOT NULL , PRIMARY KEY (id))';
-
-        if (!empty($queries)) {
-            $db->beginTransaction();
-            try {
-                foreach ($queries as $q) {
-                    $db->query($q);
-                }
-
-                $db->commit();
-            } catch (\Exception $e) {
-                $db->rollback();
-
-                throw $e;
-            }
-        }
     }
 }
